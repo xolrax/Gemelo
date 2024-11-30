ccf383f2ecbe37ab9b6cd72bd9127de25c08e07f
xolrax <xalvear@gmail.com>
2024-11-30 18:48:28 -0300

    Update Extract_text_pdf.py

--- Extract_text_pdf.py
+++ Extract_text_pdf.py
@@ -47,6 +47,48 @@ def extract_text_with_details(pdf_path):
         print(f"Error al procesar el PDF: {e}")
         return None
 
+
+def extract_text_with_details_block(pdf_path):
+    """
+    Extrae líneas de texto de un archivo PDF junto con sus detalles como tipo de fuente y tamaño, 
+    procesando solo los bloques que contengan texto.
+
+    Args:
+        pdf_path (str): Ruta al archivo PDF.
+
+    Returns:
+        list: Lista de detalles de texto (líneas, fuente, tamaño, posición).
+    """
+    try:
+        text_details = []
+        char_count = 0  # Contador de caracteres incremental
+
+        # Abre el documento PDF con un contexto
+        with fitz.open(pdf_path) as pdf_document:
+            # Itera sobre todas las páginas
+            for page_num in range(pdf_document.page_count):
+                page = pdf_document.load_page(page_num)
+                text_blocks = page.get_text("blocks")  # Obtiene el texto estructurado como bloques
+                
+                for block in text_blocks:
+                    # Procesa solo los bloques que contengan texto y no imágenes u otros elementos
+                    if block[4] != "":
+                        detail = {
+                            "page": page_num + 1,
+                            "block_bbox": block[1],  # Posición del bloque
+                            "text": block[4],         # Texto del bloque
+                            "char_count": char_count + len(block[4])  # Contador de caracteres acumulado
+                        }
+                        char_count += len(block[4])
+                        text_details.append(detail)
+
+        return text_details
+
+    except (FileNotFoundError, ValueError, TypeError) as e:
+        print(f"Error al procesar el PDF: {e}")
+        return None
+
+
 def save_text_details_to_json(text_details, output_path):
     """
     Guarda la lista de detalles de texto en un archivo JSON.
